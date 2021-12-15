c619db5c34978f68342739ca9cd3d1aa114ef331
jjm2473 <1129525450@qq.com>
2021-12-15 14:33:19 +0800

    target/realtek: remove nonexistent board from opkg/distfeeds.conf
    (cherry picked from commit 47b057004c275abfb5ee5d7a1a3a03befde8fab5)

--- target/linux/realtek/base-files.mk
+++ target/linux/realtek/base-files.mk
@@ -2,3 +2,13 @@
 define Package/base-files/install-target
 	chmod 0400 $(1)/etc/public.key
 endef
+
+define Package/base-files/postinst
+#!/bin/sh
+
+[ -n "$${IPKG_INSTROOT}" ] && {
+	FILE="$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
+	$(SED) '/\/targets\/realtek\/rtd129x\//d' $${FILE}
+}
+
+endef
