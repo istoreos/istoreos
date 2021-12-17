19cf90379bebea73f85ef7633b5fbce82281f64a
jjm2473 <1129525450@qq.com>
2021-12-17 11:00:04 +0800

    target/realtek: try to connect 192.168.100.1 on flashing or reseting
    (cherry picked from commit 920ed0e25b1f409a74fa4ad537db817db396486f)

--- target/linux/realtek/base-files.mk
+++ target/linux/realtek/base-files.mk
@@ -7,8 +7,10 @@ define Package/base-files/postinst
 #!/bin/sh
 
 [ -n "$${IPKG_INSTROOT}" ] && {
-	FILE="$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
-	$(SED) '/\/targets\/realtek\/rtd129x\//d' $${FILE}
+	$(SED) '/\/targets\/realtek\/rtd129x\//d' "$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
+	$(SED) 's/"192.168.1.1"/"192.168.100.1"/' \
+		"$${IPKG_INSTROOT}/usr/lib/lua/luci/controller/admin/system.lua" \
+		"$${IPKG_INSTROOT}/etc/board.d/99-default_network"
 }
 
 endef
