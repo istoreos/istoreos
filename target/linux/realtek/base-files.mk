12de64b703c2e70672cb25c55a9d199c02f6a7fd
jjm2473 <1129525450@qq.com>
2021-12-17 22:12:06 +0800

    target/realtek: fix imagebuilder
    (cherry picked from commit 137f9bd1c805f366751cfe9b90b63739c26babde)

--- target/linux/realtek/base-files.mk
+++ target/linux/realtek/base-files.mk
@@ -6,11 +6,12 @@ endef
 define Package/base-files/postinst
 #!/bin/sh
 
+HOST_SED="$(subst ${STAGING_DIR_HOST},$${STAGING_DIR_HOST},$(SED))"
+
 [ -n "$${IPKG_INSTROOT}" ] && {
-	$(SED) '/\/targets\/realtek\/rtd129x\//d' "$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
-	$(SED) 's/"192.168.1.1"/"192.168.100.1"/' \
+	$${HOST_SED} '/\/targets\/realtek\/rtd129x\//d' "$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
+	$${HOST_SED} 's/"192.168.1.1"/"192.168.100.1"/' \
 		"$${IPKG_INSTROOT}/usr/lib/lua/luci/controller/admin/system.lua" \
 		"$${IPKG_INSTROOT}/etc/board.d/99-default_network"
 }
-
 endef
