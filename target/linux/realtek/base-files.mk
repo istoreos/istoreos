c323541d73850e809397f31c61a8f3e93c9bb919
jjm2473 <1129525450@qq.com>
2022-01-10 11:07:58 +0800

    target/realtek: allow multiple hd idle
    (cherry picked from commit ed2aad5d0a53e1606b8323b4a18a263af8e955d4)

--- target/linux/realtek/base-files.mk
+++ target/linux/realtek/base-files.mk
@@ -13,5 +13,7 @@ HOST_SED="$(subst ${STAGING_DIR_HOST},$${STAGING_DIR_HOST},$(SED))"
 	$${HOST_SED} 's/"192.168.1.1"/"192.168.100.1"/' \
 		"$${IPKG_INSTROOT}/usr/lib/lua/luci/controller/admin/system.lua" \
 		"$${IPKG_INSTROOT}/etc/board.d/99-default_network"
+
+	$${HOST_SED} 's/s\.anonymous = true/s\.anonymous = true\ns\.addremove = true/' $${IPKG_INSTROOT}/usr/lib/lua/luci/model/cbi/hd_idle.lua
 }
 endef
