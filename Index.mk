88ffd7d2c81537a4eb26cd38a3a0ef1c539b773c
jjm2473 <1129525450@qq.com>
2022-05-12 16:29:03 +0800

    rtd129x: update

--- Index.mk
+++ Index.mk
@@ -1,115 +1,25 @@
-# Makefile for OpenWrt
-#
-# Copyright (C) 2007 OpenWrt.org
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
-#
-
 TOPDIR:=${CURDIR}
-LC_ALL:=C
-LANG:=C
-TZ:=UTC
-export TOPDIR LC_ALL LANG TZ
 
-empty:=
-space:= $(empty) $(empty)
-$(if $(findstring $(space),$(TOPDIR)),$(error ERROR: The path to the OpenWrt directory must not include any spaces))
-
-world:
-
-DISTRO_PKG_CONFIG:=$(shell which -a pkg-config | grep -E '\/usr' | head -n 1)
 export PATH:=$(TOPDIR)/staging_dir/host/bin:$(PATH)
 
 include rules.mk
-include $(INCLUDE_DIR)/depends.mk
-include $(INCLUDE_DIR)/subdir.mk
-include target/Makefile
-# OLD_ARCH_PACKAGES:=$(ARCH_PACKAGES)
-# ARCH_PACKAGES:=x86_64
-include package/Makefile
-# ARCH_PACKAGES:=$(OLD_ARCH_PACKAGES)
-include tools/Makefile
-include toolchain/Makefile
+
+FEEDS_AVAILABLE:=${INDEX_FEEDS}
 
 ARCH_PACKAGES:=${INDEX_ARCH}
 PACKAGE_SUBDIRS:=$(foreach FEED,$(FEEDS_AVAILABLE),$(OUTPUT_DIR)/packages/$(ARCH_PACKAGES)/$(FEED))
 
-$(toolchain/stamp-compile): $(tools/stamp-compile)
-$(target/stamp-compile): $(toolchain/stamp-compile) $(tools/stamp-compile) $(BUILD_DIR)/.prepared
-$(package/stamp-compile): $(target/stamp-compile) $(package/stamp-cleanup)
-$(package/stamp-install): $(package/stamp-compile)
-$(target/stamp-install): $(package/stamp-compile) $(package/stamp-install)
-check: $(tools/stamp-check) $(toolchain/stamp-check) $(package/stamp-check)
-
-printdb:
-	@true
-
-prepare: $(target/stamp-compile)
-
-clean: FORCE
-	rm -rf $(BUILD_DIR) $(STAGING_DIR) $(BIN_DIR) $(OUTPUT_DIR)/packages/$(ARCH_PACKAGES) $(BUILD_LOG_DIR) $(TOPDIR)/staging_dir/packages
-
-dirclean: clean
-	rm -rf $(STAGING_DIR_HOST) $(STAGING_DIR_HOSTPKG) $(TOOLCHAIN_DIR) $(BUILD_DIR_BASE)/host $(BUILD_DIR_BASE)/hostpkg $(BUILD_DIR_TOOLCHAIN)
-	rm -rf $(TMP_DIR)
-
-ifndef DUMP_TARGET_DB
-$(BUILD_DIR)/.prepared: Makefile
-	@mkdir -p $$(dirname $@)
-	@touch $@
-
-tmp/.prereq_packages: .config
-	unset ERROR; \
-	for package in $(sort $(prereq-y) $(prereq-m)); do \
-		$(_SINGLE)$(NO_TRACE_MAKE) -s -r -C package/$$package prereq || ERROR=1; \
-	done; \
-	if [ -n "$$ERROR" ]; then \
-		echo "Package prerequisite check failed."; \
-		false; \
-	fi
-	touch $@
-endif
-
-# check prerequisites before starting to build
-prereq: $(target/stamp-prereq) tmp/.prereq_packages
-	@if [ ! -f "$(INCLUDE_DIR)/site/$(ARCH)" ]; then \
-		echo 'ERROR: Missing site config for architecture "$(ARCH)" !'; \
-		echo '       The missing file will cause configure scripts to fail during compilation.'; \
-		echo '       Please provide a "$(INCLUDE_DIR)/site/$(ARCH)" file and restart the build.'; \
-		exit 1; \
-	fi
-
-$(BIN_DIR)/profiles.json: FORCE
-	$(if $(CONFIG_JSON_OVERVIEW_IMAGE_INFO), \
-		WORK_DIR=$(BUILD_DIR)/json_info_files \
-			$(SCRIPT_DIR)/json_overview_image_info.py $@ \
-	)
-
-json_overview_image_info: $(BIN_DIR)/profiles.json
-
-checksum: FORCE
-	$(call sha256sums,$(BIN_DIR),$(CONFIG_BUILDBOT))
-
-buildversion: FORCE
-	$(SCRIPT_DIR)/getver.sh > $(BIN_DIR)/version.buildinfo
-
-feedsversion: FORCE
-	$(SCRIPT_DIR)/feeds list -fs > $(BIN_DIR)/feeds.buildinfo
-
-diffconfig: FORCE
-	mkdir -p $(BIN_DIR)
-	$(SCRIPT_DIR)/diffconfig.sh > $(BIN_DIR)/config.buildinfo
-
-buildinfo: FORCE
-	$(_SINGLE)$(SUBMAKE) -r diffconfig buildversion feedsversion
-
-prepare: .config $(tools/stamp-compile) $(toolchain/stamp-compile)
-	$(_SINGLE)$(SUBMAKE) -r buildinfo
-
-world: prepare $(target/stamp-compile) $(package/stamp-compile) $(package/stamp-install) $(target/stamp-install) FORCE
-	$(_SINGLE)$(SUBMAKE) -r package/index
-	$(_SINGLE)$(SUBMAKE) -r json_overview_image_info
-	$(_SINGLE)$(SUBMAKE) -r checksum
-
-.PHONY: clean dirclean prereq prepare world package/symlinks package/symlinks-install package/symlinks-clean
+curdir:=package
+$(curdir)/index: FORCE
+	@echo Generating package index for $(ARCH_PACKAGES)...
+	@for d in $(PACKAGE_SUBDIRS); do ( \
+		mkdir -p $$d; \
+		cd $$d || continue; \
+		$(SCRIPT_DIR)/ipkg-make-index.sh . 2>&1 > Packages.manifest; \
+		grep -vE '^(Maintainer|LicenseFiles|Source|SourceName|Require|SourceDateEpoch)' Packages.manifest > Packages; \
+		case "$$(((64 + $$(stat -L -c%s Packages)) % 128))" in 110|111) \
+			$(call ERROR_MESSAGE,WARNING: Applying padding in $$d/Packages to workaround usign SHA-512 bug!); \
+			{ echo ""; echo ""; } >> Packages;; \
+		esac; \
+		gzip -9nc Packages > Packages.gz; \
+	); done
