e799c080ace1fa38a63b8ba5633e907bd81c2e4c
jjm2473 <1129525450@qq.com>
2022-05-15 15:26:02 +0800

    target/rtd129x: remove kmod-ipt-offload from default packages

--- target/linux/realtek/rtd129x/target.mk
+++ target/linux/realtek/rtd129x/target.mk
@@ -4,8 +4,10 @@ include $(TOPDIR)/rules.mk
 ARCH:=aarch64
 BOARDNAME:=RTD129x NAS
 CPU_TYPE:=cortex-a53
+DEVICE_TYPE:=basic
 FEATURES+=ext4
 DEFAULT_PACKAGES+=rtk-init rtd129x_env e2fsprogs resize2fs ethtool mkf2fs partx-utils block-mount openssl-util
+DEFAULT_PACKAGES+=dnsmasq firewall ip6tables iptables odhcp6c odhcpd-ipv6only ppp ppp-mod-pppoe
 
 KERNELNAME:=Image dtbs
 
