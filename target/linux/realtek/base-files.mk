0269cfa89ded09d7ff20ba62f6d1ff370e9795f9
jjm2473 <1129525450@qq.com>
2022-05-15 21:48:00 +0800

    istoreos: keep core repo in distfeeds.conf

--- target/linux/realtek/base-files.mk
+++ target/linux/realtek/base-files.mk
@@ -2,3 +2,16 @@
 define Package/base-files/install-target
 	chmod 0400 $(1)/etc/public.key
 endef
+
+define Package/base-files/postinst
+#!/bin/sh
+
+HOST_SED="$(subst ${STAGING_DIR_HOST},$${STAGING_DIR_HOST},$(SED))"
+HOST_LN="$(subst ${STAGING_DIR_HOST},$${STAGING_DIR_HOST},$(LN))"
+
+[ -n "$${IPKG_INSTROOT}" ] && {
+	$${HOST_SED} '/^src\/gz openwrt_core /d' "$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
+}
+true
+
+endef
