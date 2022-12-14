6c0a7f4ee29d0db4e964ee79dfaa47ea6d7369e1
jjm2473 <1129525450@qq.com>
2022-12-14 14:19:20 +0800

    target: remove core feed on unsupported target

--- target/linux/realtek/base-files.mk
+++ target/linux/realtek/base-files.mk
@@ -10,7 +10,7 @@ HOST_SED="$(subst ${STAGING_DIR_HOST},$${STAGING_DIR_HOST},$(SED))"
 HOST_LN="$(subst ${STAGING_DIR_HOST},$${STAGING_DIR_HOST},$(LN))"
 
 [ -n "$${IPKG_INSTROOT}" ] && {
-	$${HOST_SED} '/^src\/gz openwrt_core /d' "$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
+	$${HOST_SED} '/targets\/realtek\/rtd129x/d' "$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
 }
 true
 
