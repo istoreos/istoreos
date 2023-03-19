d6c59ce1e234501e2e12f1663b42c2d92fb7c704
jjm2473 <1129525450@qq.com>
2023-03-19 22:07:11 +0800

    target/linux: fix kmod-hwmon-pwmfan depends

--- package/kernel/linux/modules/hwmon.mk
+++ package/kernel/linux/modules/hwmon.mk
@@ -397,7 +397,7 @@ define KernelPackage/hwmon-pwmfan
   KCONFIG:=CONFIG_SENSORS_PWM_FAN
   FILES:=$(LINUX_DIR)/drivers/hwmon/pwm-fan.ko
   AUTOLOAD:=$(call AutoLoad,60,pwm-fan)
-  $(call AddDepends/hwmon, +PACKAGE_kmod-thermal:kmod-thermal)
+  $(call AddDepends/hwmon, +PACKAGE_kmod-thermal:kmod-thermal +ALL_KMODS:kmod-thermal)
 endef
 
 define KernelPackage/hwmon-pwmfan/description
