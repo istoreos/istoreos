a7a783579614606513e6dd3785967a1427f81005
jjm2473 <1129525450@qq.com>
2023-04-11 16:29:32 +0800

    target/rockchip: add h66k

--- target/linux/rockchip/image/rk3568.mk
+++ target/linux/rockchip/image/rk3568.mk
@@ -6,6 +6,11 @@ define Device/rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3568 | pine64-img | gzip | append-metadata
 endef
 
+define Device/rk3568_combined
+  IMAGE/combined.img.gz := boot-combined | boot-script rk3568-cb | pine64-img | gzip | append-metadata
+  IMAGES := combined.img.gz
+endef
+
 define Device/fastrhino_common
 $(call Device/rk3568)
   DEVICE_VENDOR := FastRhino
@@ -26,21 +31,33 @@ TARGET_DEVICES += fastrhino_r68s
 
 define Device/fastrhino_r6xs
 $(call Device/fastrhino_common)
+$(call Device/rk3568_combined)
   DEVICE_MODEL := R68s/R66s combined
   SUPPORTED_DEVICES += fastrhino,r66s fastrhino,r68s
   DEVICE_DTS := rk3568-r66s rk3568-r68s
-  IMAGE/sysupgrade.img.gz := boot-combined | boot-script rk3568-cb | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += fastrhino_r6xs
 
-define Device/hinlink_opc-h68k
+define Device/hinlink_common
 $(call Device/rk3568)
   DEVICE_VENDOR := HINLINK
-  DEVICE_MODEL := OPC-H68K
-  SUPPORTED_DEVICES += hinlink,opc-h68k
   DEVICE_PACKAGES := kmod-r8125 kmod-nvme kmod-scsi-core
 endef
+
+define Device/hinlink_opc-h68k
+$(call Device/hinlink_common)
+  DEVICE_MODEL := OPC-H68K
+endef
 TARGET_DEVICES += hinlink_opc-h68k
+
+define Device/hinlink_opc-h6xk
+$(call Device/hinlink_common)
+$(call Device/rk3568_combined)
+  DEVICE_MODEL := OPC-H68K/H66K combined
+  SUPPORTED_DEVICES += hinlink,opc-h66k hinlink,opc-h68k
+  DEVICE_DTS := rk3568-opc-h66k rk3568-opc-h68k
+endef
+TARGET_DEVICES += hinlink_opc-h6xk
 
 define Device/friendlyarm_nanopi-r5s
 $(call Device/rk3568)
