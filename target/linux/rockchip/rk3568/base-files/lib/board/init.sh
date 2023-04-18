56a5f7446f62cd045519a58ae1919d9bd5a72ff8
jjm2473 <1129525450@qq.com>
2023-04-18 18:10:48 +0800

    target/rockchip: rk3568 set interfaces smp mark

--- target/linux/rockchip/rk3568/base-files/lib/board/init.sh
+++ target/linux/rockchip/rk3568/base-files/lib/board/init.sh
@@ -18,9 +18,9 @@ set_iface_cpumask() {
     local irq
     local seconds
 
-    [ -z "${device}" ] && device="$interface"
+    [[ -z "${device}" || "${device}" = "${interface}-0" ]] && ip link set dev "${interface}" up
 
-    ip link set dev "$interface" up
+    [[ -z "${device}" ]] && device="$interface"
 
     for seconds in $(seq 0 2); do
         irq=$(grep -m1 " ${device}\$" /proc/interrupts | sed -n -e 's/^ *\([^ :]\+\):.*$/\1/p')
@@ -35,9 +35,10 @@ set_iface_cpumask() {
 board_fixup_iface_name() {
     local device
     case $(board_name) in
-    fastrhino,r66s)
-        device="$(get_iface_device eth0)"
-        if [[ "$device" != "0001:11:00.0" ]]; then
+    fastrhino,r66s|\
+    hinlink,opc-h66k)
+        device="$(get_iface_device eth1)"
+        if [[ "$device" = "0001:11:00.0" ]]; then
             rename_iface eth0 lan1
             rename_iface eth1 eth0
             rename_iface lan1 eth1
@@ -45,41 +46,47 @@ board_fixup_iface_name() {
         ;;
     fastrhino,r68s)
         device="$(get_iface_device eth0)"
-        if [[ "$device" != "fe010000.ethernet" ]]; then
+        if [[ "$device" = "fe010000.ethernet" ]]; then
             rename_iface eth0 wan
             rename_iface eth1 eth0
             rename_iface wan eth1
         fi
-        device="$(get_iface_device eth2)"
-        if [[ "$device" != "0002:21:00.0" ]]; then
+        device="$(get_iface_device eth3)"
+        if [[ "$device" = "0002:21:00.0" ]]; then
             rename_iface eth2 lan3
             rename_iface eth3 eth2
             rename_iface lan3 eth3
         fi
         ;;
     firefly,rk3568-roc-pc)
-        device="$(get_iface_device eth0)"
-        if [[ "$device" != "fe010000.ethernet" ]]; then
+        device="$(get_iface_device eth1)"
+        if [[ "$device" = "fe010000.ethernet" ]]; then
             rename_iface eth0 wan
             rename_iface eth1 eth0
             rename_iface wan eth1
         fi
         ;;
     friendlyelec,nanopi-r5s)
-        device="$(get_iface_device eth1)"
+        device="$(get_iface_device eth2)"
         # r5s lan1 is under pcie2x1
-        if [[ "$device" != "0000:01:00.0" ]]; then
+        if [[ "$device" = "0000:01:00.0" ]]; then
             rename_iface eth1 lan2
             rename_iface eth2 eth1
             rename_iface lan2 eth2
         fi
         ;;
     hinlink,opc-h68k)
-        device="$(get_iface_device eth0)"
-        if [[ "$device" != "fe010000.ethernet" ]]; then
+        device="$(get_iface_device eth1)"
+        if [[ "$device" = "fe010000.ethernet" ]]; then
             rename_iface eth0 wan
             rename_iface eth1 eth0
             rename_iface wan eth1
+        fi
+        device="$(get_iface_device eth3)"
+        if [[ "$device" = "0001:11:00.0" ]]; then
+            rename_iface eth2 lan3
+            rename_iface eth3 eth2
+            rename_iface lan3 eth3
         fi
         ;;
     esac
@@ -87,10 +94,6 @@ board_fixup_iface_name() {
 
 board_set_iface_smp_affinity() {
     case $(board_name) in
-    fastrhino,r68s)
-        set_iface_cpumask 2 eth0
-        set_iface_cpumask 4 eth1
-        ;;
     firefly,rk3568-roc-pc)
         set_iface_cpumask 2 eth0
         set_iface_cpumask 4 eth1
@@ -98,15 +101,25 @@ board_set_iface_smp_affinity() {
     friendlyelec,nanopi-r5s)
         set_iface_cpumask 2 eth0
         ;;
+    fastrhino,r68s|\
     hinlink,opc-h68k)
         set_iface_cpumask 1 "eth0"
         set_iface_cpumask 2 "eth1"
-        set_iface_cpumask 4 "eth2-0"
-        set_iface_cpumask 4 "eth2-16"
-        set_iface_cpumask 2 "eth2-18"
-        set_iface_cpumask 8 "eth3-0"
-        set_iface_cpumask 8 "eth3-18"
-        set_iface_cpumask 0 "eth3-16"
+        set_iface_cpumask 4 "eth2" "eth2-0"
+        set_iface_cpumask 4 "eth2" "eth2-16"
+        set_iface_cpumask 2 "eth2" "eth2-18"
+        set_iface_cpumask 8 "eth3" "eth3-0"
+        set_iface_cpumask 8 "eth3" "eth3-18"
+        set_iface_cpumask 1 "eth3" "eth3-16"
+        ;;
+    fastrhino,r66s|\
+    hinlink,opc-h66k)
+        set_iface_cpumask 4 "eth0" "eth0-0"
+        set_iface_cpumask 4 "eth0" "eth0-16"
+        set_iface_cpumask 2 "eth0" "eth0-18"
+        set_iface_cpumask 8 "eth1" "eth1-0"
+        set_iface_cpumask 8 "eth1" "eth1-18"
+        set_iface_cpumask 1 "eth1" "eth1-16"
         ;;
     esac
 }
