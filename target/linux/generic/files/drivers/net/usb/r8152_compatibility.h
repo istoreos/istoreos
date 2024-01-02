cf3090bc91c7d8b0e77289cd340285e62ead9b57
Liangbin Lian <jjm2473@gmail.com>
2024-01-02 16:58:35 +0800

    kernel: r8152 fix redefinition of 'eth_hw_addr_set'

--- target/linux/generic/files/drivers/net/usb/r8152_compatibility.h
+++ target/linux/generic/files/drivers/net/usb/r8152_compatibility.h
@@ -608,10 +608,12 @@
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5,8,0) */
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5,9,0) */
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5,12,0) */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,10,188)
 	static inline void eth_hw_addr_set(struct net_device *dev, const u8 *addr)
 	{
 		memcpy(dev->dev_addr, addr, 6);
 	}
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5,10,188) */
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5,15,0) */
 
 #ifndef FALSE
