e5f919390ee9ea485af7b6e7dfa66c70b9e0f383
Liangbin Lian <jjm2473@gmail.com>
2024-03-25 16:11:32 +0800

    target/amlogic: init

---
+++ target/linux/amlogic/image/meson.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+define Device/phicomm_n1
+  DEVICE_VENDOR := Phicomm
+  DEVICE_MODEL := N1
+  SOC := meson-gxl-s905d
+  UBOOT_DEVICE_NAME := phicomm-n1
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | aml-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152 ethtool
+endef
+TARGET_DEVICES += phicomm_n1
