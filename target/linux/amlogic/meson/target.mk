cd3543993610d246f5716e22ed5198da3c3f7e9e
Liangbin Lian <jjm2473@gmail.com>
2024-03-29 15:58:27 +0800

    target/amlogic: add kmod-fb to default packages
    show hdmi console

--- target/linux/amlogic/meson/target.mk
+++ target/linux/amlogic/meson/target.mk
@@ -2,7 +2,7 @@ ARCH:=aarch64
 CPU_TYPE:=cortex-a53
 BOARDNAME:=Meson S905x boards (64 bit)
 
-DEFAULT_PACKAGES+=ethtool parted
+DEFAULT_PACKAGES+=ethtool parted kmod-fb
 
 define Target/Description
 	Build firmware image for Amlogic S905x devices.
