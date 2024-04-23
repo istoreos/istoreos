3726849cd007e9657b593db1fe75e42f4f8d7caa
qlljcf666 <60305000+qlljcf666@users.noreply.github.com>
2024-04-23 16:11:13 +0800

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -148,6 +148,34 @@ define Device/ampedwireless_ally_common
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
+define Device/ws7915
+  $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := Huasifei
+  DEVICE_MODEL := WS7915
+  DEVICE_VARIANT := 16M
+  DEVICE_PACKAGES := kmod-ata-ahci kmod-sdhci-mt7620 kmod-usb3 \
+		kmod-usb-ledtrig-usbport \
+		kmod-mt7915e kmod-mt76 -wpad-wolfssl
+  SUPPORTED_DEVICES += ws7915 huasifei,ws7915
+endef
+TARGET_DEVICES += ws7915
+
+define Device/ws7915-32
+  $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
+  IMAGE_SIZE := 32448k
+  DEVICE_VENDOR := Huasifei
+  DEVICE_MODEL := WS7915-32
+  DEVICE_VARIANT := 32M
+  DEVICE_PACKAGES := kmod-ata-ahci kmod-sdhci-mt7620 kmod-usb3 \
+		kmod-usb-ledtrig-usbport \
+		kmod-mt7915e kmod-mt76 -wpad-wolfssl
+  SUPPORTED_DEVICES += ws7915-32 huasifei,ws7915-32
+endef
+TARGET_DEVICES += ws7915-32
+
 define Device/ampedwireless_ally-r1900k
   $(Device/ampedwireless_ally_common)
   DEVICE_MODEL := ALLY-R1900K
