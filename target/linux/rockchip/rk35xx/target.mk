f859237145bec9a09c6310c9cba7b68d555d74af
Liangbin Lian <jjm2473@gmail.com>
2024-04-24 18:18:29 +0800

    target/rk35xx: update board name

--- target/linux/rockchip/rk35xx/target.mk
+++ target/linux/rockchip/rk35xx/target.mk
@@ -1,11 +1,11 @@
 ARCH:=aarch64
 CPU_TYPE:=cortex-a53
-BOARDNAME:=RK3568/RK3588 boards (64 bit)
+BOARDNAME:=RK35xx boards (64 bit)
 FEATURES+=pwm
 
 DEFAULT_PACKAGES+=kmod-r8125 ethtool
 
 define Target/Description
-	Build firmware image for Rockchip RK3568/RK3588 devices.
+	Build firmware image for Rockchip RK3528/RK3566/RK3568/RK3588 devices.
 	This firmware features a 64 bit kernel.
 endef
