e57369dabf9628bec5cbca27c75c115c4f50d540
Liangbin Lian <jjm2473@gmail.com>
2024-09-10 17:30:11 +0800

    rkbin: update rk3568 bl31 to v1.44

--- package/boot/arm-trusted-firmware-rockchip-vendor/atf-version.mk
+++ package/boot/arm-trusted-firmware-rockchip-vendor/atf-version.mk
@@ -5,7 +5,7 @@ RK3308_DDR_UART2:=rk3308_ddr_589MHz_uart2_m0_v2.07.bin
 RK3528_ATF:=rk3528_bl31_v1.13.elf
 RK3528_DDR:=rk3528_ddr_1056MHz_v1.05.bin
 
-RK3568_ATF:=rk3568_bl31_v1.40.elf
+RK3568_ATF:=rk3568_bl31_v1.44.elf
 RK3568_DDR:=rk3568_ddr_1560MHz_v1.21.bin
 
 RK3588_ATF:=rk3588_bl31_v1.37.elf
