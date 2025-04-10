33a19e16c310210a032b260157f482c13c3df2a2
Liangbin Lian <jjm2473@gmail.com>
2025-04-10 16:37:15 +0800

    target/rockchip: add friendlyarm nanopi r6s/r6c support
    Signed-off-by: Liangbin Lian <jjm2473@gmail.com>

--- target/linux/rockchip/image/legacy.mk
+++ target/linux/rockchip/image/legacy.mk
@@ -15,3 +15,21 @@ $(call Device/Legacy/rk3588,$(1))
   DEVICE_DTS = rk3588/$$(SOC)-$(lastword $(subst _, ,$(1)))
 endef
 
+define Device/friendlyarm_nanopi-r6s
+$(call Device/Legacy/rk3588s,$(1))
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R6S/R6C
+  SUPPORTED_DEVICES += friendlyarm,nanopi-r6s friendlyarm,nanopi-r6c
+  SUPPORTED_DEVICES += friendlyelec,nanopi-r6s friendlyelec,nanopi-r6c
+  DEVICE_DTS := rk3588/rk3588s-nanopi-r6s rk3588/rk3588s-nanopi-r6c
+  BOOT_SCRIPT := rk3588-friendlyelec
+  DEVICE_PACKAGES += kmod-r8169
+endef
+
+# avoid using upstream dts
+define Device/friendlyarm_nanopi-r6c
+$(call Device/Legacy/rk3588s,$(1))
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R6C
+  DEVICE_PACKAGES += kmod-r8169
+endef
